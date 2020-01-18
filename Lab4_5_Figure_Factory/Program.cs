cd497dd09bc34c97270178b42e06863ed7998441
Patryk Święcicki <patswiecicki@gmail.com>
2020-01-18 10:42:14 +0100

    Dodaj pliki projektów.

---
+++ Lab4_5_Figure_Factory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_5_Figure_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
