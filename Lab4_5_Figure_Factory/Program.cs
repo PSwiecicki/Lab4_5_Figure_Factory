e71ade060c33015948b7653126f16162d7d1d186
Patryk Święcicki <patswiecicki@gmail.com>
2020-01-18 11:10:18 +0100

    Created Class Square

--- Lab4_5_Figure_Factory/Program.cs
+++ Lab4_5_Figure_Factory/Program.cs
@@ -4,9 +4,31 @@ namespace Lab4_5_Figure_Factory
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Square
+    {
+        //Constructors
+        public Square(double a)
+        {
+           side = a;
+        }
+
+        //Fields
+        private double side;
+
+        //Properties
+        public double perimeter { get { return side * 4; } }
+        public double field { get { return side * side; } }
+
+        //Methods
+        public override string ToString()
+        {
+            return String.Format("Kwadrat: {0}", side); 
         }
     }
 }
