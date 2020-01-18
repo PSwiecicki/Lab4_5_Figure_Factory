69e58381059bad70eb3be70185962ecc7c885aef
Patryk Święcicki <patswiecicki@gmail.com>
2020-01-18 11:33:06 +0100

    Created Class Circle

---
+++ Lab4_5_Figure_Factory/Class/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4_5_Figure_Factory.Class
+{
+    class Circle : IFigure
+    {
+        //Constructors
+        public Circle(double a)
+        {
+            radius = a;
+        }
+
+        //Fields
+        private readonly double radius;
+
+        //Properties
+        public double Perimeter { get { return 2 * Math.PI * radius; } }
+        public double Field { get { return Math.PI * radius * radius; } }
+
+        //Methods
+        public override string ToString()
+        {
+            return String.Format("Koło: {0}", radius);
+        }
+    }
+}
